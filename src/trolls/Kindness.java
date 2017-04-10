56d036e3bb5002f27127ba22e30771c3e9591ce2
merfemor <efremov.roman98@gmail.com>
2017-04-10 20:20:31 +0300

    Fix toString() in Kindness

--- src/trolls/Kindness.java
+++ src/trolls/Kindness.java
@@ -19,17 +19,17 @@ public class Kindness implements Comparable<Kindness> {
 
     @Override
     public String toString() {
-        if(kindness == DEVIL.kindness) return "DEVIL IN THE FLESH!!";
-        else if(kindness == VERY_BAD.kindness) return "very bad";
-        else if(kindness == BULLY.kindness) return "bully";
-        else if(kindness == BAD.kindness) return "bad";
-        else if(kindness == NORMAL.kindness) return "good";
-        else if(kindness == GOOD.kindness) return "beautiful";
-        else if(kindness == GREAT.kindness) return "great";
-        else if(kindness == BEAUTIFUL.kindness) return "beautiful";
-        else if(kindness == BRILLIANT.kindness) return "brilliant";
-        else if(kindness == ANGEL.kindness) return "angel";
-        else return (kindness > 0) ? "good" : "bad";
+        if(kindness <= DEVIL.kindness) return "DEVIL IN THE FLESH!!";
+        else if(kindness <= VERY_BAD.kindness) return "very bad";
+        else if(kindness <= BULLY.kindness) return "bully";
+        else if(kindness <= BAD.kindness) return "bad";
+        else if(kindness <= NORMAL.kindness) return "normal";
+        else if(kindness <= GOOD.kindness) return "good";
+        else if(kindness <= GREAT.kindness) return "great";
+        else if(kindness <= BEAUTIFUL.kindness) return "beautiful";
+        else if(kindness <= BRILLIANT.kindness) return "brilliant";
+        else if(kindness <= ANGEL.kindness) return "angel";
+        else return "MOST KIND!!";
     }
 
     @Override
